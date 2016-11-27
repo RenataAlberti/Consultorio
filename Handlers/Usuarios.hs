a7b9656edc1a9930a8e21c375dcb7359e75de69b
Caio Lima <caiolima86@gmail.com>
2016-11-27 21:15:53 +0000

    adicionando handler usuarios

---
+++ Handlers/Usuarios.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE OverloadedStrings, QuasiQuotes,
+             TemplateHaskell #-}
+ 
+module Handlers.Usuarios where
+import Yesod
+import Foundation
+import Control.Monad.Logger (runStdoutLoggingT)
+import Control.Applicative
+import Data.Text
+import Database.Persist.Postgresql
+
+<<<<<<< HEAD
+{--Teste  --}
+=======
+>>>>>>> 66168b7e15d9405bfdc42b0256eb87dcaf36a3a3
